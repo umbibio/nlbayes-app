e3c304fcf8ab7bfd05a1405376f8f7675ef7d811
Argenis Arriojas <argenis@arriojas.net>
2023-02-21 18:24:53 -0500

    Update entrypoints

--- docker_entrypoint_dash.sh
+++ docker_entrypoint_dash.sh
@@ -3,9 +3,10 @@
 if [ ! -f /.initialized ]; then
 
     echo "getting ready"
-    pip install --upgrade pip
+    pip install --no-cache-dir --upgrade pip
     pip install --no-cache-dir celery[librabbitmq,redis] pymongo numpy pandas openpyxl xlrd
     pip install --no-cache-dir dash dash-bootstrap-components dash-bootstrap-templates dash-cytoscape gunicorn
+    pip install --no-cache-dir bibtexparser
 
     touch /.initialized
 fi
--- docker_entrypoint_worker.sh
+++ docker_entrypoint_worker.sh
@@ -4,6 +4,7 @@ if [ ! -f /.initialized ]; then
     echo "getting ready"
     apt-get update
     apt-get install -y libgsl-dev
+    pip install --no-cache-dir --upgrade pip
     pip install --no-cache-dir cython cysignals
     pip install --no-cache-dir git+https://github.com/umbibio/nlbayes-python.git
     pip install --no-cache-dir celery[librabbitmq,redis] pymongo
